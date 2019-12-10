068d927c34cb6e7e56d8dcd77a947059edc17b0a
omerchoen <omerc@anyvision.co>
2019-12-10 19:37:47 +0200

    finished livedatabus

--- app/src/main/java/iam/immlkit/facedetector/model/rxbus/EventMutableLiveData.kt
+++ app/src/main/java/iam/immlkit/facedetector/model/rxbus/EventMutableLiveData.kt
@@ -5,9 +5,16 @@ import androidx.lifecycle.MutableLiveData
 
 class EventMutableLiveData<T>(value: Event<T>?) : MutableLiveData<Event<T>>(value) {
 
+
     override fun setValue(value: Event<T>?) {
         super.setValue(value)
         //mark as hasbeenhandled only after calling super and all subscribed observers hs been called
         value?.hasBeenHandled = true
+    }
+
+    override fun postValue(value: Event<T>?) {
+        super.postValue(value)
+        //execute on main thread using same executor live data is using to make sure we mark event as handled after it executed all of it observers
+        ArchTaskExecutor.getInstance().postToMainThread{value?.hasBeenHandled = true}
     }
 }
